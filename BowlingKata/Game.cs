bfc58c5d54e9f251a30df87df4b0e6bb6933bfdd
孟宣 陳 <stezen129@hotmail.com>
2019-07-08 03:05:50 +0800

    ● Move the Game class to other file

---
+++ BowlingKata/Game.cs
@@ -0,0 +1,94 @@
+namespace BowlingKata
+{
+    internal class Game
+    {
+        private int _score;
+        private int _frame;
+        private readonly int[] _scoreBox = new int[21];
+        private int count = 1;
+
+        public void Roll(int pins)
+        {
+            if (count == 12)
+            {
+                _frame += 0;
+            }
+            _score += pins;
+            _scoreBox[_frame] = pins;
+
+            if (IsStrike())
+            {
+                _score += StrikeBonus();
+            }
+            else if (IsSpare())
+            {
+                _score += SpareBonus();
+            }
+
+            NextFrame(pins);
+        }
+
+        private int SpareBonus()
+        {
+            return _scoreBox[_frame];
+        }
+
+        private int StrikeBonus()
+        {
+            if ((_scoreBox[_frame - 1] == 0 || _scoreBox[_frame] == 0) && _frame <= 18)
+            {
+                return 20;
+            }
+
+            if (IsFinal())
+            {
+                return _scoreBox[_frame];
+            }
+            return _scoreBox[_frame - 1] + _scoreBox[_frame];
+        }
+
+        private bool IsFinal()
+        {
+            return _frame > 18;
+        }
+
+        private bool IsStrike()
+        {
+            if (_frame >= 3)
+            {
+                return _scoreBox[_frame - 2] == 10 || _scoreBox[_frame - 3] == 10;
+            }
+
+            return false;
+        }
+
+        private bool IsSpare()
+        {
+            if ((_frame % 2 == 0 && _frame != 0) &&
+                (_scoreBox[_frame - 2] != 0 && _scoreBox[_frame - 1] != 0))
+            {
+                return _scoreBox[_frame - 1] + _scoreBox[_frame - 2] == 10;
+            }
+            return false;
+        }
+
+        public void NextFrame(int pins)
+        {
+            if (pins == 10 && _frame < 18)
+            {
+                _frame += 2;
+            }
+            else
+            {
+                _frame++;
+            }
+
+            count++;
+        }
+
+        public int GetScore()
+        {
+            return _score;
+        }
+    }
+}
